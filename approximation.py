e0f45b0ef2e77e0fe03971aa110dc10fbf42d6b8
weixr18 <weixr18@mails.tsinghua.edu.cn>
2020-11-15 11:46:22 +0800

    last commit

--- approximation.py
+++ approximation.py
@@ -1,18 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from interpolation import func
+from interpolation import accurate_model
 
-N = 0  # TODO: replace with real N
-I0 = 0  # TODO: replace with real I0
+N = 1000
+I0 = 1
 r_beta = 1
 
+M = 1000
 
-def get_approx_func(coeffs):
-    def func(t):
-        Ht = t  # replace with your implementation
-        return Ht
 
-    return func
+def get_approx_func(bound):
+    def approx_func(x):
+        return 1000 / (1 + 999 * np.power((1 + -x / M), M))
+
+    return approx_func
 
 
 if __name__ == "__main__":
@@ -20,11 +21,11 @@ if __name__ == "__main__":
     bound = input("Please input the error bound:")
     approx_func = get_approx_func(float(bound))
 
-    f, (ax1, ax2) = plt.subplots(1, 2)
-    t = np.arange(0, 10, 0.1)
-    I_approx = approx_func(t)
-    I = func(t)
+    x = np.arange(0, 10, 0.1)
+    I_approx = approx_func(x)
+    I = accurate_model(x)
 
-    ax1.plot(t, I)
-    ax2.plot(t, I_approx, c='r')
+    f, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.plot(x, I)
+    ax2.plot(x, I_approx, c='r')
     plt.show()
